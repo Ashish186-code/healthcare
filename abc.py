09fa6745b57f1f92353a05cef9692dc7e3b6fd39
Ashish186-code <96528269+Ashish186-code@users.noreply.github.com>
2023-05-29 21:17:19 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+def home_page():
+    st.markdown("<h1 style='text-align: center; font-size: 50px;color: yellow;'>HealthCare Guidance</h1>", unsafe_allow_html=True)
+    selected_option = st.selectbox("Select an option", ["Diseases", "Drugs", "Variants", "Genes"])
+    if st.button("Submit"):
+        st.session_state["selected_option"] = selected_option
+        st.session_state["page"] = "About"
+
+def about_page():
+    columns = st.columns(4)
+    Diseases=["abc","def","kbc"]
+    Drugs=["abc","def","kbc"]
+    Variants=["abc","def","kbc"]
+    Genes=["abc","def","kbc"]
+    st.write(f"You have selected the option: {st.session_state['selected_option']}")
+       
+    with columns[0]:
+        st.header("Diseases")
+        st.write(Diseases)
+    
+    with columns[1]:
+        st.header("Drugs")
+        st.write(Drugs)
+    
+    with columns[2]:
+        st.header("Variants")
+        st.write(Variants)
+    
+    with columns[3]:
+        st.header("Genes")
+        st.write(Genes)
+
+def main():
+    if "page" not in st.session_state:
+        st.session_state["page"] = "Home"
+
+    if st.session_state["page"] == "Home":
+        home_page()
+    elif st.session_state["page"] == "About":
+        about_page()
+
+if __name__ == "__main__":
+    main()
