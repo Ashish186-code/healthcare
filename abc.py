8b9593f65a2a6ef0be82981954aa725317a56dad
Ashish186-code <96528269+Ashish186-code@users.noreply.github.com>
2023-05-29 23:03:48 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -3,6 +3,15 @@ import streamlit as st
 def home_page():
     st.markdown("<h1 style='text-align: center; font-size: 50px;color: yellow;'>HealthCare Guidance</h1>", unsafe_allow_html=True)
     selected_option = st.selectbox("Select an option", ["Diseases", "Drugs", "Variants", "Genes"])
+    page_bg_img = '''
+        <style>
+        body {
+            background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM0bdgiorxP3EY7paJ6e8d99pYTCOXdPrYRA&usqp=CAU");
+            background-size: cover;
+        }
+        </style>
+        '''
+    st.markdown(page_bg_img, unsafe_allow_html=True)
     if st.button("Submit"):
         st.session_state["selected_option"] = selected_option
         st.session_state["page"] = "About"
